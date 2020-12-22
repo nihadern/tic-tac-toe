e7c8263bb5fe7a5ca63cc6c7b418039df1925725
nihadern <nihadkalathingal@gmail.com>
2020-12-22 16:14:48 -0500

    add comments

--- js/TicTacToe.js
+++ js/TicTacToe.js
@@ -1,33 +1,44 @@
+// enumerations of possible states of board squares
 const Players = {
   X: 1,
   O: 2,
   BLANK: 3,
 };
+
+// function to determine the next player. i.e. toggle X/O
 function nextPlayer(currentPlayer) {
   return currentPlayer === Players.X ? Players.O : Players.X;
 }
 
+// constant for score used in minimax
 const SCORE = 1;
+// size of the tic-tac-toe square board 
 let BOARD_SIZE = 4;
 
+// object reoresentation of a tic tac toe board 
 class TicTacToeBoard {
   constructor(state) {
+    // use a board state if given
     if (state) this.state = state;
     else {
+      // create an empty board
       this.state = new Array(BOARD_SIZE);
       for (let i = 0; i < BOARD_SIZE; i++)
         this.state[i] = new Array(BOARD_SIZE).fill(Players.BLANK);
     }
   }
 
+  // helper function to add a move to board
   addMove(row, col, player) {
     this.state[row][col] = player;
   }
 
+  // helper function to clear a move
   clearMove(row, col) {
     this.state[row][col] = Players.BLANK;
   }
 
+  // helper function that finds all possible moves left on board
   getChildMoves() {
     const moves = [];
     for (let i = 0; i < BOARD_SIZE; i++)
@@ -37,6 +48,7 @@ class TicTacToeBoard {
     return moves;
   }
 
+  // copies the current state of board. 2D array copy. 
   copyState() {
     const copy = new Array(BOARD_SIZE);
     for (let i = 0; i < BOARD_SIZE; i++) {
@@ -46,6 +58,7 @@ class TicTacToeBoard {
     return copy;
   }
 
+  // determines whether the current board has aby playable sqaures
   isPlayable() {
     for (let i = 0; i < BOARD_SIZE; i++)
       for (let j = 0; j < BOARD_SIZE; j++)
@@ -53,6 +66,8 @@ class TicTacToeBoard {
     return false;
   }
 
+
+  // get the winner of the board if any otherwise return null
   getWinner() {
     // check diagonal
     let temp = this.state[0][0];
@@ -105,41 +120,53 @@ class TicTacToeBoard {
   }
 }
 
+// Computer/AI agent that picks the optimal move using minimax algorithm
 class TicTacToeAgent {
   constructor(playerTag, depthLimit = Number.MAX_VALUE) {
+    // an agent has a tag: X/O 
     this.playerTag = playerTag;
+    // optional depth limit to adjust difficulty
     this.depthLimit = depthLimit;
   }
 
+  // given a board state return the most optimal move for the agent
   getBestMove(ticTacToeBoard) {
-    // get all possible
+    // get all possible moves
     let moves = ticTacToeBoard.getChildMoves();
+    // if any moves left
     if (moves.length > 0) {
+      // the move with the highest value is the best move
       let maxValue = -Number.MAX_VALUE;
       let bestMove = null;
+      //iterate through moves
       for (let i = 0; i < moves.length; i++) {
         const [row, col] = moves[i];
+        // add move to board 
         ticTacToeBoard.addMove(row, col, this.playerTag);
+        // minimax search to determine score of the move
         const moveValue = this.miniMax(
           ticTacToeBoard,
-          0,
+          0, // depth is 0 for start
           nextPlayer(this.playerTag),
-          -Number.MAX_VALUE,
-          Number.MAX_VALUE
+          -Number.MAX_VALUE, // alpha is -infinity
+          Number.MAX_VALUE // beta is infinity
         );
+        // clear the move of the board 
         ticTacToeBoard.clearMove(row, col);
-        console.log(row, col);
+        // replace the best move if the maxValue is greater 
         if (moveValue > maxValue) {
           maxValue = moveValue;
           bestMove = moves[i];
         }
       }
+      // return the best move
       return bestMove;
-    } else return null;
+    } else return null; // no moves returns null
   }
 
+  // recursive depth-limited funtion for minimax with alpha-beta pruning 
   miniMax(ticTacToeBoard, depth, playerTag, alpha, beta) {
-    // check for depth limit
+    // check for depth limit and return if depth higher than limit
     if (depth > this.depthLimit) return 0;
     const winner = ticTacToeBoard.getWinner();
     // if there is a winner return positive score if agent wins
@@ -148,14 +175,19 @@ class TicTacToeAgent {
 
     // get all possible moves
     const moves = ticTacToeBoard.getChildMoves();
+    // if no possible moves and no winner, its a draw
     if (moves.length === 0) return 0;
 
     // if the current player is agent, maximize scores
     if (playerTag === this.playerTag) {
+      // max will pick the move with the maximum value
       let maxValue = -Number.MAX_VALUE;
+      // iterate through moves
       for (let i = 0; i < moves.length; i++) {
         const [row, col] = moves[i];
+        // add move to board
         ticTacToeBoard.addMove(row, col, playerTag);
+        // compute score and replace the maxValue if higher
         maxValue = Math.max(
           maxValue,
           this.miniMax(
@@ -166,7 +198,9 @@ class TicTacToeAgent {
             beta
           )
         );
+        // remove the move 
         ticTacToeBoard.clearMove(row, col);
+        // update alpha
         alpha = Math.max(alpha, maxValue);
         // beta cutoff
         if (alpha >= beta) break;
@@ -175,10 +209,14 @@ class TicTacToeAgent {
     }
     // if the current player is not agent, minimize scores
     else {
+      // the min player will pick move which minimizes score
       let minValue = Number.MAX_VALUE;
+      // iterate through moves 
       for (let i = 0; i < moves.length; i++) {
         const [row, col] = moves[i];
+        // add the move to board
         ticTacToeBoard.addMove(row, col, playerTag);
+        // compute score and replace the minValue if lower
         minValue = Math.min(
           minValue,
           this.miniMax(
@@ -189,7 +227,9 @@ class TicTacToeAgent {
             beta
           )
         );
+        // clear the move
         ticTacToeBoard.clearMove(row, col);
+        // update beta 
         beta = Math.min(beta, minValue);
         // alpha cut-off
         if (alpha >= beta) break;
--- js/main.js
+++ js/main.js
@@ -1,24 +1,34 @@
+// game starts with X
 var currentPlayer = Players.X;
+// the board for the game
 var ticTacToeBoard = new TicTacToeBoard();
+// toggle for Human vs. AI or Human vs. Human
 var playWithAgent = true;
+// the compuer/AI agent plays second
 var agent = new TicTacToeAgent(nextPlayer(currentPlayer));
+
+// a map of symbols for display on board 
 const playerSymbols = {
   [Players.BLANK]: " ",
   [Players.X]: "X",
   [Players.O]: "O",
 };
 
+// dropdown select parser for difficulty callback function
 function depthLimit(selectionID = "difficulty") {
   const depthSelect = document.getElementById(selectionID);
   const depth = depthSelect.options[depthSelect.selectedIndex].value;
+  // parse depthlimit and create new agen with depth limit
   agent = new TicTacToeAgent(
     nextPlayer(currentPlayer),
     depth === "inf" ? Number.MAX_VALUE : depth
   );
 }
 
+// size toggele to switch between 3x3 and 4x4 boards
 function toggleSize(buttonID = "size-toggle") {
   const button = document.getElementById(buttonID);
+  // change button text and render new
   if (BOARD_SIZE === 4) {
     button.innerText = "Play 4 x 4";
     BOARD_SIZE = 3;
@@ -30,6 +40,7 @@ function toggleSize(buttonID = "size-toggle") {
   }
 }
 
+// toggles human v. human and human v. AI
 function toggleAI(buttonID = "ai-toggle") {
   const button = document.getElementById(buttonID);
   if (playWithAgent) button.innerText = "Play against Human vs. AI";
@@ -37,36 +48,52 @@ function toggleAI(buttonID = "ai-toggle") {
   playWithAgent = !playWithAgent;
 }
 
+// helper function for status display with colors for success/ failure
 function displayMessage(message, success = true, messageBoxId = "status") {
   const messageBox = document.getElementById(messageBoxId);
   messageBox.style = success ? "color:green;" : "color:red;";
   messageBox.innerText = message;
 }
 
+// renders a move onto the board
 function renderMove(row, col, player) {
   const cell = document.getElementById(`${row},${col}`);
   cell.innerText = playerSymbols[player];
 }
 
+// callback for a click on the board
 function onClickCell(e) {
+  // find the cell that was clicked
   const clickedCell = e.target;
+  // if clicked cell is not already played
   if (clickedCell.innerText === "") {
+    // set the clicked cell to be current players tag
     clickedCell.innerText = playerSymbols[currentPlayer];
+    // add the same move to board object
     ticTacToeBoard.addMove(
       clickedCell.getAttribute("row"),
       clickedCell.getAttribute("col"),
       currentPlayer
     );
+    // determine if it was a winningplay
     let winner = ticTacToeBoard.getWinner();
+    // determine if draw/ any moves left
     let playable = ticTacToeBoard.isPlayable();
+    // if it was a winning play, display winner 
     if (winner) displayMessage(`${playerSymbols[winner]} has won!`);
+    // if not winner and not playable, it's a draw
     else if (!playable) displayMessage(`It's a draw!`);
+    // check if playing with agent 
     else if (playWithAgent) {
+      // agent move
       currentPlayer = nextPlayer(currentPlayer);
+      // ask agent for best move
       let [row, col] = agent.getBestMove(ticTacToeBoard);
+      // play the best move
       ticTacToeBoard.addMove(row, col, currentPlayer);
       renderMove(row, col, currentPlayer);
       displayMessage("");
+      // check for winner/draw
       let winner = ticTacToeBoard.getWinner();
       let playable = ticTacToeBoard.isPlayable();
       if (winner) displayMessage(`${playerSymbols[winner]} has won!`);
@@ -76,22 +103,28 @@ function onClickCell(e) {
   }
 }
 
+// init function to render a new board with a board object
 function renderBoard() {
+  // init vars
   ticTacToeBoard = new TicTacToeBoard();
   depthLimit();
   displayMessage("");
   currentPlayer = Players.X;
   const board = document.getElementById("board");
+  // remove all cells
   while (board.firstChild) {
     board.removeChild(board.firstChild);
   }
+  // recreate new board
   for (let i = 0; i < BOARD_SIZE; i++) {
     let row = document.createElement("tr");
     for (let j = 0; j < BOARD_SIZE; j++) {
       let cell = document.createElement("td");
+      // add row, col attr to query later
       cell.setAttribute("row", i);
       cell.setAttribute("col", j);
       cell.id = `${i},${j}`;
+      // hook funtion
       cell.addEventListener("click", onClickCell);
       row.appendChild(cell);
     }
